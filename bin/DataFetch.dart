bef4c23c02051ffd439d0660c380edd4728c6af0
Azmaeen Mahtab <mahtabazmaeen7@gmail.com>
2025-12-19 17:55:44 +0600

    data fetch using http

---
+++ bin/DataFetch.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+void fetchData() async {
+
+  var url =
+  Uri.https('jsonplaceholder.typicode.com', '/posts/1' );
+
+  var response = await http.get(url);
+
+  print(response.body);
+
+}
+
+void main(){
+
+  fetchData();
+}
