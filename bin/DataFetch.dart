e03290801cdcc149ec4d44d9fb15d84922ab8462
Azmaeen Mahtab <mahtabazmaeen7@gmail.com>
2026-01-03 17:26:57 +0600

    data fetch using http

--- bin/DataFetch.dart
+++ bin/DataFetch.dart
@@ -1,17 +1,20 @@
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
-void fetchData() async {
+// void fetchData() async {
+//
+//   var url =
+//   Uri.https('jsonplaceholder.typicode.com', '/posts/1' );
+//
+//   var response = await http.get(url);
+//
+//   print(response.body);
+//
+// }
+//
+// void main(){
+//
+//   fetchData();
+// }
 
-  var url =
-  Uri.https('jsonplaceholder.typicode.com', '/posts/1' );
 
-  var response = await http.get(url);
-
-  print(response.body);
-
-}
-
-void main(){
-
-  fetchData();
-}
+//data fetch is not necessary for this project
